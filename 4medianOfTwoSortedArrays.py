d19b9e9c585ef93bb6b8198764ecd15d1ad29a8c
Murray <59850021+LarryEinstein@users.noreply.github.com>
2023-02-03 10:23:15 +0000

    Update 4medianOfTwoSortedArrays.py

--- 4medianOfTwoSortedArrays.py
+++ 4medianOfTwoSortedArrays.py
@@ -1,30 +1,31 @@
-# #4medianOfTwoSortedArrays
+#4medianOfTwoSortedArrays
 
-# class Solution(object):
-# 	def findMedianSortedArray(self, nums1, nums2):
-# 		
+class Solution(object):
+	def findMedianSortedArray(nums1, nums2):
+ 		
 
-array1 = [1, 2, 4, 5]
-array2 = [3, 6, 9, 12, 13, 13]
+		# nums1 = [1, 2, 4, 5]
+		# nums2 = [3, 6, 9, 12, 13, 13, 35, 27]
 
-combined_array = array1 + array2
+		combined_array = nums1 + nums2
 
-print(combined_array)
+		print(combined_array)
 
-combined_array.sort()
+		combined_array.sort()
 
-print(combined_array)
+		print(combined_array)
 
-print(len(combined_array))
+		print(len(combined_array))
 
-print(combined_array[int(0)])
+		print(combined_array[int(0)])
 
 
-if len(combined_array)%2==0:
-	value = combined_array[int((len(combined_array)-1)/2)] + combined_array[int((len(combined_array))/2)]
-	median = value/2
-	print(median)
-else:
-	median = combined_array[int((len(combined_array)-1)/2)]
-	print(median)
+		if len(combined_array)%2==0:
+			value = combined_array[int((len(combined_array)-1)/2)] + combined_array[int((len(combined_array))/2)]
+			median = value/2
+			print(median)
+		else:
+			median = combined_array[int((len(combined_array)-1)/2)]
+			print(median)
 
+a1 = Solution.findMedianSortedArray(nums1 = [1, 2, 4, 5], nums2 = [3, 6, 9, 12, 13, 13, 35, 27])
